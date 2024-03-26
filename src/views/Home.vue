8184323d7304a525e47465bfa5702ba8cb099c14
Dennis Brink <dennis.brink@villaforyou.com>
2024-03-26 21:56:11 +0100

    EP-4 EP-5

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="home">
-    <Toast v-if="showToast" />
+    <!-- <Transition name="xtoast">
+      <Toast v-if="showToast" />
+    </Transition> -->
+    <Transition name="xtoasta">
+      <Toast v-if="showToast" />
+    </Transition>    
     <Todos @badValue="triggerToast" />
     <Transition name="diok">
       <div v-if="showdiv">
@@ -54,4 +59,76 @@ export default {
   transition: opacity 2s ease;
 }
 
+/* not used when using a animation */
+/* enter classes */
+.xtoast-enter-from {
+  opacity: 0;
+  transform: translateY(-60px); /* initially we set it off the screen with a transform and negative pixels */
+}
+.xtoast-enter-to {
+  opacity: 1;
+  transform: translateY(0);  /* make it appear at its original location from the top at the end of the transition */
+}
+
+.xtoast-enter-active {
+  transition: all 0.5s ease;
+}
+
+.xtoasta-enter-active {
+  animation: wobble 0.5s ease; /* animation is in keyframes */
+}
+
+/* leave classes */
+.xtoasta-leave-from {
+  opacity: 1;
+  transform: translateY(0); /* initially we we acknowledge the original location */
+}
+.xtoasta-leave-to {
+  opacity: 0;
+  transform: translateY(-60px); /* make it end up 60 pixels off the screen at the end of the transition*/ 
+}
+.xtoasta-leave-active {
+  transition: all 0.3s ease;
+}
+
+/* leave classes */
+.xtoast-leave-from {
+  opacity: 1;
+  transform: translateY(0); /* initially we we acknowledge the original location */
+}
+.xtoast-leave-to {
+  opacity: 0;
+  transform: translateY(-60px); /* make it end up 60 pixels off the screen at the end of the transition*/ 
+}
+.xtoast-leave-active {
+  transition: all 0.3s ease;
+}
+
+/* use this deatiled animation sequence in active class */
+/* depending on where you use it, this replaces the from, to and active classes because it incorporates it */
+@keyframes wobble {
+  0% {
+    opacity: 0;
+    transform: translateY(-60px);
+  }
+  50% {
+    transform: translateY(0);
+    opacity: 1;
+  }
+  60% {
+    transform: translateX(8px);
+  }
+  70% {
+    transform: translateX(-8px);
+  }
+  80% {
+    transform: translateX(4px);
+  }
+  90% {
+    transform: translateX(-4px);
+  }
+  100% {
+    transform: translateX(0);
+  }  
+}
 </style>
